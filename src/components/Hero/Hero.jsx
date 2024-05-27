ef26ece100fe232aa8bbb0abb0b04de68364c932
Eduardo Santiago <eduardosantiago_@hotmail.com>
2024-05-27 19:37:52 -0300

    feat: navigation function and time effect

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,8 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const Hero = () => {
+  const [bgColor, setBgColor] = useState("bg-green-600");
+
+  useEffect(() => {
+    const checkTime = () => {
+      const now = new Date();
+      const hours = now.getHours();
+      const minutes = now.getMinutes();
+
+      const currentTime = hours * 60 + minutes;
+      const startTime = 13 * 60 + 30;
+      const endTime = 19 * 60;
+
+      if (currentTime < startTime || currentTime > endTime) {
+        setBgColor("bg-red-600");
+      } else {
+        setBgColor("bg-green-600");
+      }
+    };
+
+    checkTime();
+    const intervalId = setInterval(checkTime, 60000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
   return (
     <div
+      id="home"
       className="relative h-[60vh] w-full bg-cover bg-center"
       style={{ backgroundImage: "url('./images/coffee-beans.jpg')" }}
     >
@@ -10,18 +36,16 @@ const Hero = () => {
         <img
           src="./images/coffee-cup.jpg"
           alt="Coffe Cup"
-          className="h-40 w-40 mt-10 rounded-full shadow-lg object-cover"
+          className="h-40 w-40 mt-10 rounded-full shadow-lg object-cover hover:scale-110 duration-200"
         />
         <h1 className="text-white text-4xl mt-5 mb-2 font-bold ">
           Coffee Shop
         </h1>
-
-        <span className="text-white  font-medium">
+        <span className="text-white font-medium">
           Rua ficticia, Gramado - RS
         </span>
-
         <span
-          className="bg-green-600 px-4 py-1 rounded-l mt-4 text-white  font-medium"
+          className={`${bgColor} px-4 py-1 rounded-l mt-4 text-white font-medium`}
           id="date-span"
         >
           Ter a Dom - 13:30 as 19:00
--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -8,11 +8,11 @@ import {
 
 const Menu = ({ skills }) => {
   return (
-    <div>
+    <div className="px-4 md:px-6 lg:px-16">
       <h2 className="text-2xl md:text-5xl font-bold text-center mt-9 mb-6">
         Cardápio
       </h2>
-      <section>
+      <section id="coffee">
         <main className="flex">
           {MenuCoffees.map((category) => (
             <div key={category.title}>
@@ -24,12 +24,15 @@ const Menu = ({ skills }) => {
                     className="bg-white flex p-4 rounded-lg shadow-md "
                     key={index}
                   >
-                    <img
-                      src={item.img}
-                      alt={item.coffee}
-                      className="w-40 h-40 object-cover rounded-md mb-2"
-                    />
-                    <div className="m-6">
+                    <div className="flex-shrink-0">
+                      <img
+                        src={item.img}
+                        alt={item.coffee}
+                        className="w-40 h-40 object-cover rounded-md "
+                      />
+                    </div>
+
+                    <div className="ml flex-grow m-2">
                       <h3 className="text-lg mb-2 font-semibold">
                         {item.coffee}
                       </h3>
@@ -43,8 +46,8 @@ const Menu = ({ skills }) => {
           ))}
         </main>
       </section>
-
-      <section>
+      <hr className="border-t border-gray-300 my-8" />
+      <section id="drinks">
         <main className="flex">
           {MenuDrinks.map((category) => (
             <div key={category.title}>
@@ -56,12 +59,14 @@ const Menu = ({ skills }) => {
                     className="bg-white flex p-4 rounded-lg shadow-md "
                     key={index}
                   >
-                    <img
-                      src={item.img}
-                      alt={item.drink}
-                      className="w-40 h-40 object-cover rounded-md mb-2"
-                    />
-                    <div className="m-6">
+                    <div className="flex-shrink-0">
+                      <img
+                        src={item.img}
+                        alt={item.drink}
+                        className="w-40 h-40 object-cover rounded-md mb-2"
+                      />
+                    </div>
+                    <div className="ml flex-grow m-2">
                       <h3 className="text-lg mb-2 font-semibold">
                         {item.drink}
                       </h3>
@@ -75,8 +80,8 @@ const Menu = ({ skills }) => {
           ))}
         </main>
       </section>
-
-      <section>
+      <hr className="border-t border-gray-300 my-8" />
+      <section id="candy">
         <main className="flex">
           {MenuCandy.map((category) => (
             <div key={category.title}>
@@ -88,12 +93,14 @@ const Menu = ({ skills }) => {
                     className="bg-white flex p-4 rounded-lg shadow-md "
                     key={index}
                   >
-                    <img
-                      src={item.img}
-                      alt={item.candy}
-                      className="w-40 h-40 object-cover rounded-md mb-2"
-                    />
-                    <div className="m-6">
+                    <div className="flex-shrink-0">
+                      <img
+                        src={item.img}
+                        alt={item.candy}
+                        className="w-40 h-40 object-cover rounded-md mb-2"
+                      />
+                    </div>
+                    <div className="ml flex-grow m-2">
                       <h3 className="text-lg mb-2 font-semibold">
                         {item.candy}
                       </h3>
@@ -107,8 +114,8 @@ const Menu = ({ skills }) => {
           ))}
         </main>
       </section>
-
-      <section>
+      <hr className="border-t border-gray-300 my-8" />
+      <section id="savory">
         <main className="flex">
           {MenuSavory.map((category) => (
             <div key={category.title}>
@@ -120,12 +127,14 @@ const Menu = ({ skills }) => {
                     className="bg-white flex p-4 rounded-lg shadow-md "
                     key={index}
                   >
-                    <img
-                      src={item.img}
-                      alt={item.savory}
-                      className="w-40 h-40 object-cover rounded-md mb-2"
-                    />
-                    <div className="m-6">
+                    <div className="flex-shrink-0">
+                      <img
+                        src={item.img}
+                        alt={item.savory}
+                        className="w-40 h-40 object-cover rounded-md mb-2"
+                      />
+                    </div>
+                    <div className="ml flex-grow m-2">
                       <h3 className="text-lg mb-2 font-semibold">
                         {item.savory}
                       </h3>
