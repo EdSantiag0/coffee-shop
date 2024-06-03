f802fefdf85f5ae6b2f116bcfe85a3a3b09dd883
Eduardo Santiago <eduardosantiago_@hotmail.com>
2024-06-03 20:43:44 -0300

    feat: filter implemented

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import MenuCategory from "./MenuCategory";
 import {
   MenuCoffees,
@@ -8,32 +8,80 @@ import {
 } from "../../utils/data";
 
 const Menu = () => {
+  const [categoryFilter, setCategoryFilter] = useState("all");
+
+  const getItemsByCategory = () => {
+    switch (categoryFilter) {
+      case "coffees":
+        return [{ title: "Cafés", items: MenuCoffees }];
+      case "drinks":
+        return [{ title: "Bebidas", items: MenuDrinks }];
+      case "candy":
+        return [{ title: "Doces", items: MenuCandy }];
+      case "savory":
+        return [{ title: "Salgados", items: MenuSavory }];
+      case "all":
+        return [
+          { title: "Cafés", items: MenuCoffees },
+          { title: "Bebidas", items: MenuDrinks },
+          { title: "Doces", items: MenuCandy },
+          { title: "Salgados", items: MenuSavory },
+        ];
+      default:
+        return [];
+    }
+  };
+
+  const categories = getItemsByCategory();
+
   return (
     <div className="px-4 md:px-6 lg:px-16 font-sans">
       <h2 className="text-2xl md:text-5xl font-bold text-center mt-9 mb-6 font-serif">
         Cardápio
       </h2>
-      <section id="coffee">
-        <MenuCategory title="Cafés" items={MenuCoffees} />
-      </section>
+      <div className="flex justify-center space-x-4 mb-6">
+        <button
+          onClick={() => setCategoryFilter("all")}
+          className="px-4 py-2  text-white bg-[rgba(120,60,0,0.8)] rounded-md hover:bg-[rgba(175,115,55,0.8)] transition-colors"
+        >
+          All
+        </button>
+        <button
+          onClick={() => setCategoryFilter("coffees")}
+          className="px-4 py-2 text-white bg-[rgba(120,60,0,0.8)] rounded-md hover:bg-[rgba(175,115,55,0.8)] transition-colors"
+        >
+          Coffees
+        </button>
+        <button
+          onClick={() => setCategoryFilter("drinks")}
+          className="px-4 py-2  text-white bg-[rgba(120,60,0,0.8)] rounded-md hover:bg-[rgba(175,115,55,0.8)] transition-colors"
+        >
+          Drinks
+        </button>
+        <button
+          onClick={() => setCategoryFilter("candy")}
+          className="px-4 py-2  text-white bg-[rgba(120,60,0,0.8)] rounded-md hover:bg-[rgba(175,115,55,0.8)] transition-colors"
+        >
+          Candy
+        </button>
+        <button
+          onClick={() => setCategoryFilter("savory")}
+          className="px-4 py-2  text-white bg-[rgba(120,60,0,0.8)] rounded-md hover:bg-[rgba(175,115,55,0.8)] transition-colors"
+        >
+          Savory
+        </button>
+      </div>
 
-      <hr className="border-t border-gray-300 my-8" />
-
-      <section id="drinks">
-        <MenuCategory title="Bebidas" items={MenuDrinks} />
-      </section>
-
-      <hr className="border-t border-gray-300 my-8" />
-
-      <section id="candy">
-        <MenuCategory title="Doces" items={MenuCandy} />
-      </section>
-
-      <hr className="border-t border-gray-300 my-8" />
-
-      <section id="savory">
-        <MenuCategory title="Salgados" items={MenuSavory} />
-      </section>
+      <div className="menu-items">
+        {categories.map((category, index) => (
+          <div key={index}>
+            <MenuCategory title={category.title} items={category.items} />
+            {index < categories.length - 1 && (
+              <hr className="border-t border-gray-300 my-8" />
+            )}
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <div className="bg-[rgba(123,63,0,0.8)] text-white p-4 flex items-center justify-between fixed top-0 w-full h-14 z-10">
+    <div className="bg-[rgba(120,60,0,0.8)] text-white p-4 flex items-center justify-between fixed top-0 w-full h-14 z-10">
       <div className="flex  items-center">
         <img
           src="./images/coffee-cups-logo.png"
